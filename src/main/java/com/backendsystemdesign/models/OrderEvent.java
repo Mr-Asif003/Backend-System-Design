2a6471145fca71e20032fdec606601c64871d274
Asif Khan <asifkhan.tech.2003@gmail.com>
2026-05-26 01:59:14 +0530

    Add Redisson:-For Distribution Lock (RLock)

--- src/main/java/com/backendsystemdesign/models/OrderEvent.java
+++ src/main/java/com/backendsystemdesign/models/OrderEvent.java
@@ -1,7 +1,15 @@
 package com.backendsystemdesign.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderEvent {
+
+    private String orderId;
+
+    private String status;
 }
